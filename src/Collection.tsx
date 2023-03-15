be7c843ad57e8f316ab679c71ea3a4c299424dc6
Pawel-Szydlo <pawel.szydlo.99@gmail.com>
2023-03-15 15:19:10 +0100

    fix randomize

--- src/Collection.tsx
+++ src/Collection.tsx
@@ -1,11 +1,5 @@
 import { Button } from '@mui/material'
-import {
-  devnetConnection,
-  JsonRpcProvider,
-  MoveCallTransaction,
-  SuiJsonValue,
-  UnserializedSignableTransaction
-} from '@mysten/sui.js'
+import { devnetConnection, JsonRpcProvider, UnserializedSignableTransaction } from '@mysten/sui.js'
 import { DEFAULT_GAS_BUDGET } from './App'
 import { NightlyWalletAdapter } from './nightly'
 import { fenecImages } from './utils/const'
@@ -19,23 +13,23 @@ export const Collection: React.FC<ICollection> = props => {
   const sui = new JsonRpcProvider(devnetConnection)
   const DEV_PACKAGE_MOVE_NFT_ADDRESS = '0x2'
   const randomNumber = Math.floor(Math.random() * 100000)
-  const transaction: UnserializedSignableTransaction = {
-    kind: 'moveCall',
-    data: {
-      packageObjectId: DEV_PACKAGE_MOVE_NFT_ADDRESS,
-      module: 'devnet_nft',
-      function: 'mint',
-      typeArguments: [],
-      arguments: [
-        `Nightly NFT #${randomNumber}`,
-        'Created by Nightly for testing (https://wallet.nightly.app/)',
-        fenecImages[Math.floor(Math.random() * fenecImages.length)]
-      ],
-      gasBudget: DEFAULT_GAS_BUDGET
-    }
-  }
 
   const signeAndSend = async () => {
+    const transaction: UnserializedSignableTransaction = {
+      kind: 'moveCall',
+      data: {
+        packageObjectId: DEV_PACKAGE_MOVE_NFT_ADDRESS,
+        module: 'devnet_nft',
+        function: 'mint',
+        typeArguments: [],
+        arguments: [
+          `Nightly NFT #${randomNumber}`,
+          'Created by Nightly for testing (https://wallet.nightly.app/)',
+          fenecImages[Math.floor(Math.random() * fenecImages.length)]
+        ],
+        gasBudget: DEFAULT_GAS_BUDGET
+      }
+    }
     const signetTxParse = await props.NightlySui.signAndExecuteTransaction(transaction)
 
     const id = signetTxParse?.certificate.transactionDigest
